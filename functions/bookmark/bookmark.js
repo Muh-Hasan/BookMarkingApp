2682279d573897f3f1ab69c212d09bc5c99e5e09
Muh-Hasan <muh.hasan.git@gmail.com>
2020-11-08 01:17:32 +0500

    qurey done

--- functions/bookmark/bookmark.js
+++ functions/bookmark/bookmark.js
@@ -1,17 +1,56 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+const { ApolloServer, gql } = require("apollo-server-lambda")
+const faunadb = require("faunadb")
+q = faunadb.query
+
+require("dotenv").config()
+
+const client = new faunadb.Client({
+  secret: process.env.DB_SECRET,
+})
+const typeDefs = gql`
+  type Query {
+    bookmarks: [Bookmark!]
   }
+  type Bookmark {
+    id: ID!
+    url: String!
+    title: String!
+    description: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    bookmarks: async () => {
+      try {
+        const result = await client.query(
+          q.Map(
+            q.Paginate(q.Documents(q.Collection("bookmarks"))),
+            q.Lambda(x => q.Get(x))
+          )
+        )
+        const data = result.data.map(t => {
+          return {
+            id: t.ref.id,
+            url: t.data.url,
+            title: t.data.title,
+            description: t.data.description,
+          }
+        })
+        return data
+      } catch (error) {
+        console.log(error)
+        return error.toString()
+      }
+    },
+  },
 }
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+const handler = server.createHandler()
 
 module.exports = { handler }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
 import React from "react"
+import { useQuery } from '@apollo/client'
+import gql from 'graphql-tag'
+
+const getBookmarks = gql`
+{
+  bookmarks{
+    id
+    url
+    title
+    description
+  }
+}
+`
 
 export default function Home() {
-  return <div>Hello world!</div>
+  
+  const {loading , error , data} = useQuery(getBookmarks)
+  console.log(data);
+  
+  return (
+    <div>
+      <div>Hello world!</div>
+
+    </div>
+  )
 }
